a71ecd76060b03480b2ea1264ca4f6e54d122c61
André Mello <andre.tgmello@gmail.com>
2019-10-18 07:38:38 -0300

    feat: add another option for scraping data
    get_options_dropdown now allows for the download of data not only by
    'municipio' but also by 'regiao administrativa' now.
    Also added some documentation.
    
    Signed-off-by: Andre Mello <andre.tgmello@gmail.com>

--- src/cdhudf/utils.py
+++ src/cdhudf/utils.py
@@ -22,7 +22,7 @@ def get_default_driver(tmp_path='/tmp/cdhu'):
     prefs = {'download.default_directory' : tmp_path}
     chrome_options.add_experimental_option('prefs', prefs)
     chrome_options.binary_location = '/usr/bin/chromium-browser'
-#    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
 
     # Enable headless downloading
@@ -33,19 +33,25 @@ def get_default_driver(tmp_path='/tmp/cdhu'):
 
     return driver
 
-def get_mun(driver=None):
-    """Gets available 'municipios'.
+def get_options_dropdown(driver=None, category='mun'):
+    """Gets available options from dropdown menu.
+
+    List of available 'municipios' or 'regiões administrativas'.
 
     Parameters
     ----------
     driver : optional, selenium.webdriver
         The desired selenium driver.
+    category : optional, string
+        Either 'mun' for 'municipio' or 'radm' for
+        'região administrativa'
 
     Returns
     -------
     array
         Array of 'municipios'.
     """
+
     if driver == None:
         driver = get_default_driver()
 
@@ -57,14 +63,33 @@ def get_mun(driver=None):
         print(e)
         driver.quit()
 
-    id_municipio_dropdown = driver.\
-                find_element_by_id('_ProducaoHabitacional_WAR_ProducaoHabitacional_:form:municipio')
-    select = Select(id_municipio_dropdown)
+    if category == 'mun':
+        id_dropdown = driver.\
+            find_element_by_id('_ProducaoHabitacional_WAR_ProducaoHabitacional_:form:municipio')
+    elif category == 'radm':
+        id_dropdown = driver.\
+            find_element_by_id('_ProducaoHabitacional_WAR_ProducaoHabitacional_:form:regiaoAdministrativa')
+
+    select = Select(id_dropdown)
     options = [opt.text for opt in select.options]
     options = options[1:]
     driver.close()
 
     return options
 
-def get_unidecode_dict(mun_arr):
-    return dict((uni.unidecode(val), val) for val in mun_arr)
+def get_unidecode_dict(arr):
+    """Helper function
+
+    Makes dictionary of unidecoded strings.
+
+    Parameters
+    ----------
+    arr : array
+        String array.
+
+    Returns
+    -------
+    dict
+        Dictionary of unidecoded strings.
+    """
+    return dict((uni.unidecode(val), val) for val in arr)
